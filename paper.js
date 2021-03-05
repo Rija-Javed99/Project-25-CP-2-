5d9388a009b09c0848c336ae3de0d67b65b6ca50
Rija-Javed99 <73756430+Rija-Javed99@users.noreply.github.com>
2021-03-05 16:43:57 +0530

    Update paper.js

--- paper.js
+++ paper.js
@@ -2,18 +2,37 @@ class paper
 {
 	constructor(x,y,r)
 	{
-		//add properties
+		var options={
+			isStatic:false,
+			restitution:0.3,
+			friction:0,
+			density:1.2
+
+			
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
 		
-//load image 
-
-
-		//add to world
+		//load image
+		
+		
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+		World.add(world, this.body);
 
 	}
 	display()
 	{
 			
-			//display properties
+			var paperpos=this.body.position;		
+
+			push()
+			translate(paperpos.x, paperpos.y);
+			rectMode(CENTER)
+			fill(255,0,255)
+			imageMode(CENTER);
+			image(this.image, 0,0,this.r, this.r)
+			pop()
 			
 	}
 
