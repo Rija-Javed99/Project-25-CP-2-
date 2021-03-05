f5091b3475c46af538162aac7b94dfab89d8b11f
Rija-Javed99 <73756430+Rija-Javed99@users.noreply.github.com>
2021-03-05 16:40:13 +0530

    Update dustbin.js

--- dustbin.js
+++ dustbin.js
@@ -2,11 +2,15 @@ class dustbin
 {
 	constructor(x,y)
 	{
-		//add properties
+		this.x=x;
+		this.y=y;
+		this.dustbinWidth=200;
+		this.dustbinHeight=213;
+		this.wallThickness=20;
+		
 		//load image
-
-
-
+		
+		
 		this.bottomBody=Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness, {isStatic:true})
 		this.leftWallBody=Bodies.rectangle(this.x-this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
 		
@@ -20,7 +24,43 @@ class dustbin
 
 	}
 	display()
-	{//display properties of all three sides of the dustbin
+	{
+			var posBottom=this.bottomBody.position;
+			var posLeft=this.leftWallBody.position;
+			var posRight=this.rightWallBody.position;
+
+			
+
+			push()
+			translate(posLeft.x, posLeft.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			angleMode(RADIANS)
+			fill(255)
+			rotate(this.angle)
+			//rect(0,0,this.wallThickness, this.dustbinHeight);
+			pop()
+
+			push()
+			translate(posRight.x, posRight.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			angleMode(RADIANS)
+			fill(255)
+			rotate(-1*this.angle)
+			//rect(0,0,this.wallThickness, this.dustbinHeight);
+			pop()
+
+			push()
+			translate(posBottom.x, posBottom.y+10);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			angleMode(RADIANS)
+			fill(255)
+			imageMode(CENTER);
+			image(this.image, 0,-this.dustbinHeight/2,this.dustbinWidth, this.dustbinHeight)
+			//rect(0,0,this.dustbinWidth, this.wallThickness);
+			pop()
 			
 	}
 
